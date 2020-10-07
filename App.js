0fc89bd1d31daeb9c8a051248f4d2a79a2c7cca6
sorahiro0830 <kattyamu0830@gmail.com>
2020-10-07 23:34:14 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -4,6 +4,7 @@
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -11,12 +12,28 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SingupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const App = createStackNavigator({
+  Login: { screen: LoginScreen },
+  Signup: { screen: SingupScreen },
   Home: { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit: { screen: MemoEditScreen },
-  Login: { screen: LoginScreen },
-  Singup: { screen: SingupScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memot',
@@ -28,6 +45,8 @@ const App = createStackNavigator({
       paddingBottom: 20,
     },
     headerTitleAlign: 'center',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
   },
 });
 
